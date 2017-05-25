8dc2113c0c05af48bf9c8f79000d87f21eba2808
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-05-25 19:09:18 +0100

    Added confusion matrix, comments and solved some problems

--- testUtils.py
+++ testUtils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import itertools
 
 from scipy import sparse
 
@@ -14,6 +14,14 @@ from keras.optimizers import SGD, Adam, Nadam
 from functools import partial
 
 seed = 42
+
+
+def savefig_and_close(fig, figname, path='', bbox_extra_artists=None):
+    filename = os.path.join(path, figname)
+    fig.savefig(filename, bbox_extra_artists=bbox_extra_artists,
+                bbox_inches='tight')
+    fig.clear()
+    plt.close(fig)
 
 
 def binarizeWeakLabels(z, c):
@@ -127,8 +135,8 @@ def plot_data(x, y, loc='best', save=True, title='data', cmap='Paired'):
     cmap = cm.get_cmap(cmap)
     for i, y_i in enumerate(classes):
 
-        ax.scatter(x[y==y_i, 0], x[y==y_i, 1], c=cmap(i/n_c), s=30,
-                   edgecolors=None, cmap=cmap, alpha=.8, lw=0.1,
+        ax.scatter(x[(y == y_i).flatten(), 0], x[(y == y_i).flatten(), 1],
+                   c=cmap(i/n_c), s=30, edgecolors=None, alpha=.8, lw=0.1,
                    label='{}'.format(y_i))
     ax.set_xlabel('$x_0$')
     ax.set_ylabel('$x_1$')
@@ -138,4 +146,116 @@ def plot_data(x, y, loc='best', save=True, title='data', cmap='Paired'):
     ax.grid(True)
     if save:
         savefig(fig)
+    return fig
+
+
+class MyFloat(float):
+    def _remove_leading_zero(self, value, string):
+        if 1 > value > -1:
+            string = string.replace('0', '', 1)
+        return string
+
+    def __str__(self):
+        string = super(MyFloat, self).__str__()
+        return self._remove_leading_zero(self, string)
+
+    def __format__(self, format_string):
+        string = super(MyFloat, self).__format__(format_string)
+        return self._remove_leading_zero(self, string)
+
+
+# TODO use this or other heatmap to visualize confusion matrix
+def plot_df_heatmap(df, normalize=None, title='Heat-map',
+                    cmap=plt.cm.Blues, colorbar=False):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+
+    normalize : 'rows', 'cols' (default=None)
+    """
+    rows = df.index.values
+    columns = df.columns.values
+    M = df.values
+
+    if normalize == 'rows':
+        M = M.astype('float') / M.sum(axis=1)[:, np.newaxis]
+    if normalize == 'cols':
+        M = M.astype('float') / M.sum(axis=0)[np.newaxis, :]
+
+    h_size = len(columns)*.5 + 2
+    v_size = len(rows)*.5 + 2
+    fig = plt.figure(figsize=(h_size, v_size))
+    ax = fig.add_subplot(111)
+    im = ax.imshow(M, interpolation='nearest', cmap=cmap)
+    if colorbar:
+        fig.colorbar(im)
+    ax.set_title(title)
+    column_tick_marks = np.arange(len(columns))
+    ax.set_xticks(column_tick_marks)
+    ax.set_xticklabels(columns, rotation=45, ha='right')
+    row_tick_marks = np.arange(len(rows))
+    ax.set_yticks(row_tick_marks)
+    ax.set_yticklabels(rows)
+
+    thresh = np.nanmin(M) + ((np.nanmax(M)-np.nanmin(M)) / 2.)
+    are_ints = df.dtypes[0] in ['int', 'int32', 'int64']
+    for i, j in itertools.product(range(M.shape[0]), range(M.shape[1])):
+        # fontsize is adjusted for different number of digits
+        if are_ints:
+            ax.text(j, i, M[i, j], horizontalalignment="center",
+                    verticalalignment="center", color="white" if M[i, j] >
+                    thresh else "black")
+        else:
+            if np.isfinite(M[i, j]):
+                ax.text(j, i, '{:0.2f}'.format(MyFloat(M[i, j])),
+                        horizontalalignment="center",
+                        verticalalignment="center",
+                        color="white" if M[i, j] > thresh else "black")
+
+    ax.set_ylabel(df.index.name)
+    ax.set_xlabel(df.columns.name)
+    fig.tight_layout()
+    return fig
+
+def plot_heatmap(M, columns=None, rows=None, cmap=plt.cm.Blues, colorbar=False,
+        fig=None, title='Heat-map'):
+    if columns is None:
+        columns = [str(i) for i in range(M.shape[1])]
+    if rows is None:
+        rows = [str(i) for i in range(M.shape[0])]
+
+    h_size = len(columns)*.5 + 2
+    v_size = len(rows)*.5 + 2
+
+    if fig is None:
+        fig = plt.figure(figsize=(h_size, v_size))
+
+    ax = fig.add_subplot(111)
+    im = ax.imshow(M, interpolation='nearest', cmap=cmap)
+    if colorbar:
+        fig.colorbar(im)
+    ax.set_title(title)
+    column_tick_marks = np.arange(len(columns))
+    ax.set_xticks(column_tick_marks)
+    ax.set_xticklabels(columns, rotation=45, ha='right')
+    row_tick_marks = np.arange(len(rows))
+    ax.set_yticks(row_tick_marks)
+    ax.set_yticklabels(rows)
+
+    thresh = np.nanmin(M) + ((np.nanmax(M)-np.nanmin(M)) / 2.)
+    are_ints = M.dtype in ['int', 'int32', 'int64']
+    for i, j in itertools.product(range(M.shape[0]), range(M.shape[1])):
+        # fontsize is adjusted for different number of digits
+        if are_ints:
+            ax.text(j, i, M[i, j], horizontalalignment="center",
+                    verticalalignment="center", color="white" if M[i, j] >
+                    thresh else "black")
+        else:
+            if np.isfinite(M[i, j]):
+                ax.text(j, i, '{:0.2f}'.format(MyFloat(M[i, j])),
+                        horizontalalignment="center",
+                        verticalalignment="center",
+                        color="white" if M[i, j] > thresh else "black")
+
+    fig.tight_layout()
     return fig
--- tests.py
+++ tests.py
@@ -13,6 +13,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 
 from testUtils import create_model
 from testUtils import plot_data
+from testUtils import plot_heatmap
 
 from testData import load_toy_example
 from testData import load_blobs
@@ -23,6 +24,28 @@ from diary import Diary
 seed = 42
 
 def test_1(load_data):
+    """ Trains a Feed-fordward neural network using cross-validation
+
+    The training and validation is done in the validation set using the true
+    labels
+
+    Parameters
+    ----------
+        load_data: function
+            Function that returns a training and validation set on the form
+            X_train: ndarray (n_train_samples, n_features)
+            Z_train: ndarray (n_train_samples, n_classes)
+                Weak labels in binary as a one-hot encoding
+            z_train: ndarray (n_train_samples, )
+                Weak labels as integers
+            X_val: ndarray (n_val_samples, n_features)
+            Z_val: ndarray (n_val_samples, n_classes)
+            z_val: ndarray (n_val_samples, )
+            Y_val: ndarray (n_val_samples, n_classes)
+                True labels in binary as a encoding one-hot encoding
+            y_val: ndarray (n_val_samples, )
+                True labels as integers
+    """
     # Test performance on validation true labels
     # ## Create a Diary for all the logs and results
     diary = Diary(name='test_1', path='results', overwrite=False,
@@ -94,7 +117,29 @@ def test_1(load_data):
 
 
 def test_2(load_data):
-    # Make Grid searches on validation set
+    """ Makes a Grid search on the hyperparameters of a Feed-fordwared neural
+        network
+
+    The training and validation is done in the validation set using the true
+    labels
+
+    Parameters
+    ----------
+        load_data: function
+            Function that returns a training and validation set on the form
+            X_train: ndarray (n_train_samples, n_features)
+            Z_train: ndarray (n_train_samples, n_classes)
+                Weak labels in binary as a one-hot encoding
+            z_train: ndarray (n_train_samples, )
+                Weak labels as integers
+            X_val: ndarray (n_val_samples, n_features)
+            Z_val: ndarray (n_val_samples, n_classes)
+            z_val: ndarray (n_val_samples, )
+            Y_val: ndarray (n_val_samples, n_classes)
+                True labels in binary as a encoding one-hot encoding
+            y_val: ndarray (n_val_samples, )
+                True labels as integers
+    """
     # ## Create a Diary for all the logs and results
     diary = Diary(name='test_1', path='results', overwrite=False,
                   image_format='png', fig_format='svg')
@@ -156,30 +201,73 @@ def test_2(load_data):
 
 
 def test_3(load_data):
-    # Test of training with the weak labels and test on the validation set
+    """ Trains a Feed-fordward neural network using cross-validation
+
+    The training is done with the weak labels on the training set and
+    the model is evaluated with the true labels in the validation set
+
+    Parameters
+    ----------
+        load_data: function
+            Function that returns a training and validation set on the form
+            X_train: ndarray (n_train_samples, n_features)
+            Z_train: ndarray (n_train_samples, n_classes)
+                Weak labels in binary as a one-hot encoding
+            z_train: ndarray (n_train_samples, )
+                Weak labels as integers
+            X_val: ndarray (n_val_samples, n_features)
+            Z_val: ndarray (n_val_samples, n_classes)
+            z_val: ndarray (n_val_samples, )
+            Y_val: ndarray (n_val_samples, n_classes)
+                True labels in binary as a encoding one-hot encoding
+            y_val: ndarray (n_val_samples, )
+                True labels as integers
+    """
+    # Test performance on validation true labels
+    # ## Create a Diary for all the logs and results
+    diary = Diary(name='test_1', path='results', overwrite=False,
+                  image_format='png', fig_format='svg')
+    diary.add_notebook('dataset')
+    diary.add_notebook('model')
+    diary.add_notebook('validation')
+
+    # Test for the validation error with the true labels
     X_train, Z_train, z_train, X_val, Z_val, z_val, Y_val, y_val = load_data()
 
-    seed = 42
     n_s = X_train.shape[0]
     n_f = X_train.shape[1]
     n_c = Y_val.shape[1]
 
     print("Samples = {}\nFeatures = {}\nClasses = {}".format(n_s, n_f, n_c))
+    diary.add_entry('dataset', ['n_samples', n_s, 'n_features', n_f,
+                                'n_classes', n_c])
+
+    # If dimension is 2, we draw a scatterplot
+    if n_f >= 2:
+        fig = plot_data(X_val, y_val, save=False, title='True labels')
+        diary.save_figure(fig, filename='true_labels')
+
+        fig = plot_data(X_val, z_val, save=False, title='Weak labels')
+        diary.save_figure(fig, filename='weak_labels')
+
 
     params = {'input_dim': n_f,
-             'output_size': n_c,
-             'optimizer': 'sgd',
-             'loss': 'mean_squared_error',
-             'init': 'glorot_uniform',
-             'lr': 1.0,
-             'momentum': 0.5,
-             'decay': 0.5,
-             'nesterov': True,
-             'epochs': 200,
-             'batch_size': 100,
-             'verbose': 1,
-             'seed': seed
-             }
+              'output_size': n_c,
+              'optimizer': 'rmsprop', # 'adadelta' 'adagrad' 'sgd' 'rmsprop'
+              # 'brier_score' 'w_brier_score' 'categorical_crossentropy' 'mean_squared_error'
+              'loss': 'categorical_crossentropy',
+              'init': 'glorot_uniform',
+              'lr': 1.0,
+              'momentum': 0.5,
+              'decay': 0.5,
+              'nesterov': True,
+              'epochs': 100,
+              'batch_size': 100,
+              'verbose': 1,
+              'seed': seed
+              }
+
+    diary.add_entry('model', params)
 
     make_arguments = {key: value for key, value in params.iteritems()
                                 if key in inspect.getargspec(create_model)[0]}
@@ -191,18 +279,23 @@ def test_3(load_data):
                                if key in inspect.getargspec(model.fit)[0]}
 
     if sparse.issparse(X_train):
-        X_train = X_train[:10000].toarray()
-        Z_train = Z_train[:10000]
-        z_train = z_train[:10000]
+        X_train = X_train.toarray()
+        Z_train = Z_train
+        z_train = z_train
         X_val = X_val.toarray()
 
     model.fit(X_train, Z_train, **fit_arguments)
 
     q = model.predict_proba(X_val)
-    predictions = q.argmax(axis=1)
+    y_pred = q.argmax(axis=1)
 
-    acc = accuracy_score(y_val, predictions)
+    acc = accuracy_score(y_val, y_pred)
+    print("#####")
     print("Accuracy = {}".format(acc))
+    cm = confusion_matrix(y_val, y_pred)
+    print("Confusion matrix: \n{}".format(cm))
+    fig = plot_heatmap(cm, title='Confusion matrix')
+    diary.save_figure(fig, filename='confusion_matrix')
 
 
 def test_4(load_data):
@@ -238,9 +331,9 @@ def test_4(load_data):
     acc = accuracy_score(Z_train.argmax(axis=1), predictions)
     print("Accuracy = {}".format(acc))
 
+
 def test_1a():
     # Working
-    # FIXME investigate the seed
     test_1(load_toy_example)
 
 
@@ -256,12 +349,24 @@ def test_2a():
     # Working
     test_2(load_toy_example)
 
+
 def test_2b():
     # Working
     test_2(load_blobs)
 
 
+def test_3a():
+    # Working
+    test_3(load_toy_example)
+
+
+def test_3b():
+    test_3(load_blobs)
+
+
+def test_3c():
+    test_3(load_webs)
 
 if __name__ == '__main__':
-    test_2b()
+    test_3c()
     #main()
