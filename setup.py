295cf65abd686870a5bc0d594a0f228811836828
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-05-25 12:52:14 +0100

    Added installation python file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='recybels',
+      version='1.0',
+      description=('Python package to RECYcle and aggregate laBELS with '
+                   'different levels of quality.'),
+      author='Jesus Cid-Sueiro, Raul Santos-Rodriguez, Miquel Perello-Nieto',
+      author_email='perello.nieto@gmail.com',
+      url='https://github.com/perellonieto/RecyclingLabels',
+      packages=['recybels'],
+     )
