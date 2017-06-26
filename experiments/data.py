81bde244750464c066a982c45182b9f8d72db350
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-06-26 12:07:33 +0100

    Added iris with weak and true labels

--- experiments/data.py
+++ experiments/data.py
@@ -4,9 +4,15 @@ from numpy.random import RandomState
 
 from sklearn.utils import shuffle
 from sklearn.datasets import make_blobs
+from sklearn.datasets import load_iris
 from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import StratifiedShuffleSplit
 
 from sklearn.model_selection import train_test_split
+
+from wlc.WLweakener import computeM
+from wlc.WLweakener import generateWeak
+from wlc.WLweakener import binarizeWeakLabels
 
 
 def load_webs(seed=None):
@@ -72,3 +78,42 @@ def load_blobs(n_samples=1000, n_features=2, n_classes=6, seed=None):
     X_train, X_val, Z_train, Z_val, z_train, z_val, Y_train, Y_val, y_train, y_val = train_test_split(X, Z, z, Y, y, test_size=0.5, random_state=seed)
 
     return (X_train, Z_train, z_train), (X_val, Z_val, z_val, Y_val, y_val), None
+
+
+def make_weak_true_partition(M, X, y, true_size=0.1, random_state=0):
+    n_c = len(np.unique(y))
+    classes = range(0,n_c)
+    assert(n_c == np.max(y)+1)
+    Y = label_binarize(y, classes)
+    z = generateWeak(y, M)
+    Z = binarizeWeakLabels(z, c=n_c)
+
+    # Create partition for weak and true labels
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=true_size,
+                                 random_state=random_state)
+    weak_fold, true_fold = sss.split(X, y).next()
+
+    # Weaken the true labels fold using the mixing matrix M
+    X_w = X[weak_fold]
+    z_w = z[weak_fold]
+    Z_w = Z[weak_fold]
+
+    # Select the true labels fold
+    X_t = X[true_fold]
+    z_t = z[true_fold]
+    Z_t = Z[true_fold]
+    y_t = y[true_fold]
+    Y_t = Y[true_fold]
+
+    # TODO refactor name convention of train and val, for weak and true
+    return (X_w, Z_w, z_w), (X_t, Z_t, z_t, Y_t, y_t), classes
+
+
+def load_weak_iris(method='quasi_IPL', true_size=0.1, random_state=0):
+    # Load original clean data
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    M = computeM(len(np.unique(y)), method=method)
+    return make_weak_true_partition(M, X, y, true_size=true_size,
+                                    random_state=random_state)
