003aec1458a94580defef7884553f80221cf3f57
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-08-04 18:22:47 +0100

    Removed some unused dependencies

--- experiments/analysis.py
+++ experiments/analysis.py
@@ -9,10 +9,8 @@ import numpy as np
 
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import StratifiedKFold
 # TODO Change to model_selection
-import sklearn.cross_validation as skcv
 from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -32,98 +30,6 @@ from wlc.WLweakener import computeVirtual
 from wlc.WLweakener import computeM
 from wlc.WLweakener import estimate_M
 from wlc.WLweakener import weak_to_index
-
-def n_times_k_fold_cross_val(X, V, y, classifier, n_iterations=10, k_folds=10,
-                             n_jobs=-1, fit_arguments=None,
-                             entry_notebook=None, classes=None, diary=None):
-    """Evaluates a classifier using cross-validation
-
-    Parameters
-    ----------
-    classif : object
-        This is the classifier that needs to be trained and evaluated. It needs
-        to have the following functions:
-            - fit(X,y) :
-            - predict(X) :
-            - predict_proba(X) :
-            - get_params() : All the necessary parameters to create a deep copy
-
-    X : array-like, with shape (n_samples, n_dim)
-        The data to fit.
-
-    y : array-like, with shape (n_samples, )
-        The target variable of integers. This array is used for the evaluation
-        of the model.
-
-    V : array-like, optional, with shape (n_samples, n_classes), default: 'y'
-        The virtual target variable. This array is used for the training of the
-        model.
-
-    n_sim : integer, optional, default: 1
-        The number of simulation runs.
-
-    n_jobs : integer, optional, default: 1
-        The number of CPUs to use to do the computation. -1 means 'all CPUs'
-
-    Returns
-    -------
-    predictions_training : ndarray
-        This are the predictions on the training set after training
-
-    predictions_validation : ndarray
-        This are the predictions on the validation set after training
-    """
-    n_c = V.shape[1]
-    if classes is None:
-        classes = [str(i) for i in range(n_c)]
-
-    pe_cv = [0] * n_iterations
-
-    ns = X.shape[0]
-    start = time.clock()
-    # ## Loop over simulation runs
-    for i in xrange(n_iterations):
-        X_shuff, v_shuff, y_shuff = shuffle(X, V, y, random_state=i)
-        cv_start = time.clock()
-        y_pred = skcv.cross_val_predict(classifier, X_shuff, v_shuff, cv=k_folds,
-                                        verbose=0, n_jobs=n_jobs,
-                                        fit_params=fit_arguments)
-        cv_end = time.clock()
-
-        # Estimate error rates:s
-        pe_cv[i] = float(np.count_nonzero(y_shuff != y_pred)) / ns
-
-        # ########################
-        # Ground truth evaluation:
-        #   Training with the given virtual labels (by default true labels)
-        if i == 0:
-            classifier.fit(X, V, **fit_arguments)
-            f = classifier.predict_proba(X, verbose=0)
-
-            # Then, we evaluate this classifier with all true labels
-            # Note that training and test samples are being used in this error rate
-            d = np.argmax(f, axis=1)
-            pe_tr = float(np.count_nonzero(y != d)) / ns
-
-        if entry_notebook is not None:
-            entry_notebook(row={'pe_tr': pe_tr, 'pe_cv': pe_cv[i],
-                           'cv_time': cv_end - cv_start})
-
-        stop = time.clock()
-        print(('\nAveraging {0} simulations. Estimated time to finish '
-               '{1:0.4f}s.').format(n_iterations,
-                                      (stop - start)/(i+1)*(n_iterations-i)))
-        sys.stdout.flush()
-
-        cm = confusion_matrix(y_shuff, y_pred)
-        print("Confusion matrix: \n{}".format(cm))
-        if diary is not None:
-            fig = plot_confusion_matrix(cm, columns=classes, rows=classes,
-                                        colorbar=False,
-                                        title='Confusion matrix')
-            diary.save_figure(fig, filename='confusion_matrix')
-
-    return pe_tr, pe_cv
 
 
 # TODO take a look that everything is ok
