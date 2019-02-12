33828d2802d17ec8879ae7bf452ad820b679e5fe
perellonieto <perello.nieto@gmail.com>
2019-02-12 15:43:15 +0000

    Use more general code

--- E1_full_vs_EM.sh
+++ E1_full_vs_EM.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-source ./venv3/bin/activate
+source ./venv/bin/activate
+
+declare -a r_list=(
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+)
 
 declare -a m_list=(
     noisy
@@ -18,10 +32,13 @@ declare -a m_b_list=(
     1.0
 )
 
-for m in "${m_list[@]}"
+for random_seed in "${r_list[@]}"
 do
-    for b in "${m_b_list[@]}"
+    for m in "${m_list[@]}"
     do
-        python compare_full_vs_EM.py "${m}" 1.0 "${b}"
+        for b in "${m_b_list[@]}"
+        do
+            python full_vs_EM_any_dataset.py $random_seed blobs "${m}" 1.0 "${b}"
+        done
     done
 done
