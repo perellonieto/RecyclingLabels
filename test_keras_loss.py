54abd629b104fb892727ea1931d9aff8c5d9cb18
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-02-12 17:29:58 +0000

    Loss compatible with Tensorflow

--- test_keras_loss.py
+++ test_keras_loss.py
@@ -32,10 +32,32 @@ def log_loss_np(y_true, y_pred):
     out = -y_true * np.log(y_pred)
     return np.mean(out, axis=-1)
 
+def OSL_log_loss_tensor(y_true, y_pred):
+    y_pred = K.clip(y_pred, _EPSILON, 1.0-_EPSILON)
+    y_osl = y_true * y_pred
+    y_osl_max = K.max(y_osl, axis=-1)
+    y_osl_max = K.repeat_elements(y_osl_max, y_osl.shape[-1], 0)
+    y_osl_max = K.reshape(y_osl_max, (y_osl.shape[0], y_osl.shape[-1]))
+    y_osl = K.cast(K.equal(y_osl, y_osl_max), y_pred.dtype)
+    y_osl = y_osl / K.sum(y_osl, axis=-1, keepdims=True)
+    out = -K.stop_gradient(y_osl) * K.log(y_pred)
+    return K.mean(out, axis=-1)
+
+def OSL_log_loss_np(y_true, y_pred):
+    y_pred = np.clip(y_pred, _EPSILON, 1.0-_EPSILON)
+    y_osl = y_true * y_pred
+    y_osl_max = np.reshape(np.repeat(np.max(y_osl, axis=1),
+                                           y_osl.shape[1]), (y_osl.shape[0],
+                                                             y_osl.shape[1]))
+    y_osl = np.equal(y_osl, y_osl_max)
+    y_osl = y_osl / np.sum(y_osl, axis=1, keepdims=True)
+    out = -y_osl * np.log(y_pred)
+    return np.mean(out, axis=-1)
+
 def EM_log_loss_tensor(y_true, y_pred):
     y_pred = K.clip(y_pred, _EPSILON, 1.0-_EPSILON)
     Q = y_true * y_pred
-    Z_em_train = Q / Q.sum(axis=-1, keepdims=True)
+    Z_em_train = Q / K.sum(Q, axis=-1, keepdims=True)
     out = -Z_em_train*K.log(y_pred)
     return K.mean(out, axis=-1)
 
@@ -48,7 +70,7 @@ def EM_log_loss_np(y_true, y_pred):
 
 def check_loss(_shape, _loss_tensor, _loss_np):
     if _shape == '2d':
-        shape = (6, 7)
+        shape = (13, 5)
     elif _shape == '3d':
         shape = (5, 6, 7)
     elif _shape == '4d':
@@ -57,7 +79,7 @@ def check_loss(_shape, _loss_tensor, _loss_np):
         shape = (9, 8, 5, 6, 7)
     print('Input shape = {}'.format(shape))
 
-    y_a = np.random.random(shape)
+    y_a = np.random.randint(0, 2, shape)
     y_b = np.random.random(shape)
 
     out1 = K.eval(_loss_tensor(K.variable(y_a), K.variable(y_b)))
@@ -71,36 +93,20 @@ def check_loss(_shape, _loss_tensor, _loss_np):
     print(np.linalg.norm(out1-out2))
 
 
-def test_example_loss():
-    print('Test example loss')
-    shape_list = ['2d', '3d', '4d', '5d']
+def test_loss(tensor_loss, np_loss):
+    shape_list = ['2d'] #, '3d', '4d', '5d']
     for _shape in shape_list:
-        check_loss(_shape, example_loss_tensor, example_loss_np)
-        print('======================')
-
-def test_mse_loss():
-    print('Test mean squared error')
-    shape_list = ['2d', '3d', '4d', '5d']
-    for _shape in shape_list:
-        check_loss(_shape, mse_loss_tensor, mse_loss_np)
-        print('======================')
-
-def test_log_loss():
-    print('Test log-loss')
-    shape_list = ['2d', '3d', '4d', '5d']
-    for _shape in shape_list:
-        check_loss(_shape, log_loss_tensor, log_loss_np)
-        print('======================')
-
-def test_EM_log_loss():
-    print('Test EM log-loss')
-    shape_list = ['2d', '3d', '4d', '5d']
-    for _shape in shape_list:
-        check_loss(_shape, EM_log_loss_tensor, EM_log_loss_np)
+        check_loss(_shape, tensor_loss, np_loss)
         print('======================')
 
 if __name__ == '__main__':
-    test_example_loss()
-    test_mse_loss()
-    test_log_loss()
-    test_EM_log_loss()
+    print('OSL')
+    test_loss(OSL_log_loss_tensor, OSL_log_loss_np)
+    print('Example Loss')
+    test_loss(example_loss_tensor, example_loss_np)
+    print('MSE')
+    test_loss(mse_loss_tensor, mse_loss_np)
+    print('Log-loss')
+    test_loss(log_loss_tensor, log_loss_np)
+    print('EM loss')
+    test_loss(EM_log_loss_tensor, EM_log_loss_np)
