49e2b301d59458e1484c6b27eeece1cccd04fe59
perellonieto <perello.nieto@gmail.com>
2019-02-16 12:32:22 +0000

    Removed some exp2

--- E2_full_vs_EM_webs.sh
+++ E2_full_vs_EM_webs.sh
@@ -3,7 +3,6 @@
 source ./venv/bin/activate
 
 declare -a test_list=(
-    0.6
     0.7
     0.8
     0.9
@@ -12,15 +11,13 @@ declare -a test_list=(
     0.99
 )
 
-dataset_name='webs'
-
 for random_seed in {1..100};
 do
     for test_proportion in "${test_list[@]}"
     do
-        echo "python full_vs_EM_any_dataset.py $random_seed $dataset_name $test_proportion random_weak 1.0 0.5"
-        python full_vs_EM_any_dataset.py $random_seed $dataset_name \
-            $test_proportion random_weak 1.0 0.5 > "${dataset}_${random_seed}.out" \
-                2> "${dataset}_${random_seed}.err"
+        echo "python full_vs_EM_any_dataset.py $random_seed webs $test_proportion random_weak 1.0 0.5"
+        python full_vs_EM_any_dataset.py $random_seed webs \
+            $test_proportion random_weak 1.0 0.5 > "webs_${random_seed}.out" \
+                2> "webs_${random_seed}.err"
     done
 done
