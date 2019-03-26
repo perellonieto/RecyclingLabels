230a9b91663fccd0837b80710a1da5d99410b534
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-03-26 15:58:00 +0000

    Added example 10 - Cifar10 with 3 subdatasets with random subsets of labels

---
+++ Example_10.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./venv/bin/activate
+
+declare -a m_a_list=(
+    0.9
+    0.8
+    0.7
+    0.6
+)
+
+declare -a train_prop_list=(
+    0.0
+    0.1
+    0.2
+    0.3
+    0.5
+    0.7
+    1.0
+)
+
+for random_state in {0..09};
+do
+    for prop in "${train_prop_list[@]}"
+    do
+        for a in "${m_a_list[@]}"
+        do
+            echo "
+            python Example_10_full_em_osl_cifar10_multiple_ipl.py ${random_state} \
+                 ${prop} ${a} 2>&1 > "Example_10_${random_state}_${prop}_${a}.out"
+                 "
+            python Example_10_full_em_osl_cifar10_multiple_ipl.py ${random_state} \
+                 ${prop} ${a} &> "Example_10_${random_state}_${prop}_${a}.out"
+        done
+    done
+done
