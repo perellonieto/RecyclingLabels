7e84ba0b1436bb0f1d9aa9ba1ad160887a2dafe2
perellonieto <perello.nieto@gmail.com>
2020-01-10 15:19:23 +0000

    exp07 figure size and errorbar colors

--- Example_13.sh
+++ Example_13.sh
@@ -18,14 +18,11 @@ for random_state in {0..09};
 do
     for prop in "${train_prop_list[@]}"
     do
-        for a in "${m_a_list[@]}"
-        do
-            echo "
-            python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
-                 ${prop} ${weak_true_prop} 2>&1 > "Example_08_${random_state}_${prop}_${a}.out"
-                 "
-            python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
-                 ${prop} ${weak_true_prop} &> "Example_13_${random_state}_${prop}_${a}.out"
-        done
+        echo "
+        python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
+             ${prop} ${weak_true_prop} 2>&1 > 'Example_013_${random_state}_${prop}_${weak_true_prop}.out'
+             "
+        python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
+             ${prop} ${weak_true_prop} &> "Example_13_${random_state}_${prop}_${weak_true_prop}.out"
     done
 done
