056ce512fe373becc3efdd85ded1586227672815
Miquel Perelló Nieto <perello.nieto@gmail.com>
2020-01-15 13:11:54 +0000

    Removed alpha from exp13

--- Example_13.sh
+++ Example_13.sh
@@ -16,14 +16,11 @@ for random_state in {0..09};
 do
     for prop in "${train_prop_list[@]}"
     do
-        for a in "${m_a_list[@]}"
-        do
-            echo "
-            python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
-                 ${prop} 2>&1 > "Example_08_${random_state}_${prop}_${a}.out"
-                 "
-            python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
-                 ${prop} &> "Example_13_${random_state}_${prop}_${a}.out"
-        done
+        echo "
+        python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
+             ${prop} 2>&1 > "Example_13_${random_state}_${prop}.out"
+             "
+        python Example_13_full_em_osl_mnist_complementary.py ${random_state} \
+             ${prop} &> "Example_13_${random_state}_${prop}.out"
     done
 done
